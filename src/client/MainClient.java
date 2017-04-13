fb16c0c539b067f868c9212b107497aa864d0a50
hosi <ansekal456@gmail.com>
2017-04-13 18:20:57 +0900

    ver 1.3

--- src/client/MainClient.java
+++ src/client/MainClient.java
@@ -1,19 +1,20 @@
 package client;
 
-import javax.swing.JDialog;
 import javax.swing.JFrame;
 import javax.swing.JPanel;
 public class MainClient extends JFrame{
 	JPanel p_center;
-	JPanel[] page=new JPanel[1];
-	Client_chat chat;
+	JPanel[] page=new JPanel[2];//페이지 추가
+	Client_chat chat;//채널 새창*채팅목록에서 새로열기 가능하게 바꾸기
 	
 	public MainClient(){
 		chat=new Client_chat(this);
 		p_center=new JPanel();
 		page[0]=new Client_login(this);
+		page[1]=new Client_chatList(this);
 		
 		p_center.add(page[0]);
+		p_center.add(page[1]);
 		add(p_center);
 		
 		setSize(360,590);
